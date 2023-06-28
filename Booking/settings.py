d28766177fb5e048731477f8f82b6df2190362b4
MariaKiseliova <mary.kiselyova@innowise-group.com>
2023-06-28 12:02:28 +0300

    add auth

--- Booking/settings.py
+++ Booking/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
+    'rest_framework_simplejwt',
+    'booking',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +134,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Room(models.Model):
+    number = models.IntegerField(max_length=4, unique=True)
+    cost_per_day = models.DecimalField(max_digits=7, decimal_places=2)
+    beds_numder = models.IntegerField(max_length=3)
+
+class Booking(models.Model):
+    book_start = models.DateTimeField()
+    book_end = models.DateTimeField()
+    room = models.ForeignKey(Room, on_delete=models.NOT_PROVIDED, )
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
